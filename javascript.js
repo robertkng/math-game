64cf4b63136e8ee6f8ffc0126ef71bfe0c71883a
Robert Ng <robertng85@gmail.com>
2017-08-29 21:16:47 -0400

    Add countdown

--- javascript.js
+++ javascript.js
@@ -1,15 +1,35 @@
 var playing = false;
 var score;
+var action;
+var timeRemaining;
 
-// if we click on the start reset button
+// if we click on the start/reset button
 document.getElementById("startreset").onclick = function () {
   // if we are playing
   if (playing === true) {
     // reload page
     location.reload();
   } else {
-    // if we are not playing
-    score = 0;
+    // change mode to playing
+    playing = true;
+    // if we are not playing, set score to 0
+    // score = 0;
+    score = 1;
     document.getElementById("scorevalue").innerHTML = score;
+
+    // show countdown box
+    document.getElementById("timeremaining").style.display = "block";
+
+    // change button to reset
+    document.getElementById("startreset").innerHTML = "Reset Game";
+
+    // start countdown
+    startCountdown();
   }
+}
+
+function startCountdown() {
+  action = setInterval(function() {
+
+  }, 1000);
 }
